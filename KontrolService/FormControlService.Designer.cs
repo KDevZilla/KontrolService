05d9e8870a8c5a8760df3630577ff6a09e35aa93
kdevzilla <krirk.srithawee@gmail.com>
2023-12-30 01:19:14 +0800

    Update

--- KontrolService/FormControlService.Designer.cs
+++ KontrolService/FormControlService.Designer.cs
@@ -251,6 +251,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
             this.Name = "FormControlService";
             this.Text = "Kontrol Service";
             this.Load += new System.EventHandler(this.FormControlService_Load);
--- KontrolService/frmChooseServices.Designer.cs
+++ KontrolService/frmChooseServices.Designer.cs
@@ -91,8 +91,10 @@
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.btnChooseService);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
             this.Name = "frmChooseServices";
             this.Text = "Choose Services";
             this.Load += new System.EventHandler(this.frmChooseServices_Load);
